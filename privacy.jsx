7bbed3d88e03e011ce9b1b92268f565369431b1b
istiyakansarishasanbanka-wq <istiyakansarishasanbanka@gmail.com>
2025-10-15 14:56:06 +0530

    Create privacy.jsx

---
+++ privacy.jsx
@@ -0,0 +1,9 @@
+function PrivacyPolicy() {
+  return (
+    <div className="container">
+      <h2>Privacy Policy</h2>
+      <p>Your information is secure with us. We do not share user data with anyone except for fulfilling service requests as required by law.</p>
+    </div>
+  );
+}
+export default PrivacyPolicy;
